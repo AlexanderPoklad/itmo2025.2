dc27847097f08f27d7dce005fe9147c19576abac
AlexanderPoklad <alexanderad@mail.ru>
2025-10-02 13:42:12 +0300

    домашнее задание. добавил поддержку авторизованного доступа. тесты с лекции

---
+++ spring-web/src/main/java/ru/itmo/jpa/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package ru.itmo.jpa.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/api/auth/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
